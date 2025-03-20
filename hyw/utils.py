684f41d93f77082e72253bca5e59f2077baf8564
huyiwen <1020030101@qq.com>
2025-03-20 17:44:56 +0000

    [log] fix first layer

--- hyw/utils.py
+++ hyw/utils.py
@@ -13,6 +13,10 @@ def get_hidden_states_logger(
     assert 0 <= layer_idx <= num_hidden_layers + 1  # 0 for embed_tokens, num_hidden_layers + 1 for norm & lm_head
     if log_interval is None and (layer_idx != 0 and layer_idx != num_hidden_layers + 1):
         log_interval = (num_hidden_layers - 1) // 5
+    if layer_idx == 0 or layer_idx == num_hidden_layers + 1:
+        log_interval = None
+    else:
+        layer_idx = layer_idx - 1
 
     # avoid zero division
     if log_interval == 0:
@@ -25,10 +29,10 @@ def get_hidden_states_logger(
         hidden_states = hidden_states.detach()
         if log_interval is None or layer_idx % log_interval == 0:
             hs = {
-                f"{prefix}_var/{layer_idx:02}_{name}": torch.var(hidden_states, dim=-1).mean().item(),
-                f"{prefix}_mean/{layer_idx:02}_{name}": torch.mean(hidden_states, dim=-1).mean().item(),
-                f"{prefix}_max/{layer_idx:02}_{name}": torch.max(hidden_states, dim=-1)[0].mean().item(),
-                f"{prefix}_min/{layer_idx:02}_{name}": torch.min(hidden_states, dim=-1)[0].mean().item(),
+                f"{prefix}_var/{layer_idx + 1:02}_{name}": torch.var(hidden_states, dim=-1).mean().item(),
+                f"{prefix}_mean/{layer_idx + 1:02}_{name}": torch.mean(hidden_states, dim=-1).mean().item(),
+                f"{prefix}_max/{layer_idx + 1:02}_{name}": torch.max(hidden_states, dim=-1)[0].mean().item(),
+                f"{prefix}_min/{layer_idx + 1:02}_{name}": torch.min(hidden_states, dim=-1)[0].mean().item(),
             }
             wandb.log(hs, commit=False)
 
